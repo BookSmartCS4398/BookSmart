6b88536bbe595ad87bf53fd5f00843f3d6c1b2a0
Connor Scott <cds229@txstate.edu>
2021-04-20 17:58:39 -0500

    adding tests to run, using npm test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
+import ReactDOM from 'react-dom';
 import App from './App';
+import logo from './booksmartlogo.png';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders cards without crashing', () => {
+  const div = document.createElement('div');
+  // create a react element in the div container
+  const createdComponent = ReactDOM.render(<App/>, div);
+  // Is component non-null? 
+  expect(createdComponent).toBeTruthy();
+  const cards = div.getElementsByTagName("Cards");
+  expect(cards).toBeTruthy(); //if cards exist
+  const componentIsDeleted = ReactDOM.unmountComponentAtNode(div);
+  expect(componentIsDeleted).toBeTruthy();
+ ;
+});
+test('renders, image logo component', () =>{
+  const div = document.createElement('div');
+  const createdComponent = ReactDOM.render(<App/>, div);
+  expect(createdComponent).toBeTruthy();
+  const Image = div.getElementsByTagName("img");
+  expect(Image).toBeTruthy();
+  const componentIsDeleted = ReactDOM.unmountComponentAtNode(div);
+  expect(componentIsDeleted).toBeTruthy();
 });
