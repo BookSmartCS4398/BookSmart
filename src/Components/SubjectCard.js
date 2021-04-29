e5318b4518db5aea8ff05883004854b4a855ba50
Connor Scott <cds229@txstate.edu>
2021-04-29 16:12:05 -0500

    Fixed how the link is handled for subjectCards, which had too many //

--- src/Components/SubjectCard.js
+++ src/Components/SubjectCard.js
@@ -13,8 +13,8 @@ var subCards = ({ subject }) => {
         {subject.map(book => (
           <span>
             <div className="card mb-4" key={book.key}>
-              <a href={'https://openlibrary.org/' + book.key}  target ={"_blank"} rel="noreferrer" >
-                <img src={'https://covers.openlibrary.org/b/olid/' + book.cover_edition_key + '-M.jpg'} alt="" to={'https://openlibrary.org/' + book.key} height={250}/> 
+              <a href={'https://openlibrary.org' + book.key}  target ={"_blank"} rel="noreferrer" >
+                <img src={'https://covers.openlibrary.org/b/olid/' + book.cover_edition_key + '-M.jpg'} alt="" to={'https://openlibrary.org' + book.key} height={250}/> 
               </a>
               <h2/>
                <p><b> {book.title}</b></p> {book.authors["0"].name}
